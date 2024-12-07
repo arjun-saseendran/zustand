206b850368cff9e5e6702296a03b16cb3f90cd75
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-07 21:59:04 +0530

    finish zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useAppStore } from "./store/appStore";
+import { useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputRef = useRef();
+  const username = useAppStore((state) => state.username);
+  const count = useAppStore((state) => state.count);
+  const increment = useAppStore((state) => state.increment);
+  const decrement = useAppStore((state) => state.decrement);
+  const setName = useAppStore((state) => state.setName);
+  const products = useAppStore((state) => state.products);
+  const addProduct = useAppStore((state) => state.addProduct);
+  const colors = useAppStore((state) => state.colors);
+  const voteHandler = useAppStore((state) => state.voteHandler);
+  const data = useAppStore((state)=> state.data)
+  const error = useAppStore((state)=> state.error)
+  const loading = useAppStore((state)=> state.loading)
+  const fetchData = useAppStore((state)=> state.fetchData)
 
+  useEffect(()=>{fetchData()},[])
+  if(loading){
+    return (<h5>Loading Page</h5>)
+  }
+  if(error){
+    return (<h5>{error}</h5>)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="text-center">
+        <h2 className="text-3xl text-center mb-4"></h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="card bg-base-300 p-4 flex gap-2">
+        <h2 className="text-4xl">{username}</h2>
+        <br />
+        <button
+          onClick={increment}
+          className="bg-green-600 rounded-xl text-white p-2 hover:bg-green-500"
+        >
+          Increment
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h2 className="text-4xl">{count}</h2>
+        <button
+          onClick={decrement}
+          className="bg-green-600 rounded-xl text-white p-2 hover:bg-green-500 block"
+        >
+          Decrement
+        </button>
+        <br />
+
+        <input
+          ref={inputRef}
+          className="border-2 border-blue-600 rounded-xl p-2"
+          type="text"
+        />
+        <button
+          onClick={() => setName(inputRef.current.value)}
+          className="bg-blue-600 rounded-xl text-white p-2 hover:bg-blue-500"
+        >
+          Set Name
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <button
+        onClick={() => addProduct(inputRef.current.value)}
+        className="bg-yellow-500 rounded-xl p-2 text-white"
+      >
+        Add Product
+      </button>
+
+      <u>
+        {products.map((product) => (
+          <li key={product}>{product}</li>
+        ))}
+      </u>
+      <br />
+      <div>
+        <ul>
+          {colors.map((color) => (
+            <li
+              onClick={() => voteHandler(color.id)}
+              className="bg-violet-500 cursor-pointer text-white p-2 rounded-xl inline m-2"
+              key={color.id}
+            >
+              {color.vote}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <ul>
+        {data.map((product) => <li key={product.id}>{product.title}</li>)}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/store/appStore.js
+++ src/store/appStore.js
@@ -1,13 +1,42 @@
 import { create } from "zustand";
+import { produce } from "immer";
 
 // Create coustom hook
-export const useAppStore = create(()=>{
-    return {
-        count: 0,
-        username: '',
-        isActive: true,
+export const useAppStore = create((set) => {
+  return {
+    count: 0,
+    username: "",
+    isActive: true,
+    products: ["iPhone 13", "iPhone 13 Pro", "iPhone 13 Pro Max"],
+    colors: [
+      { id: 1, title: "rose", vote: 1 },
+      { id: 2, title: "lotus", vote: 2 },
+      { id: 3, title: "jasmin", vote: 4 },
+    ],
+    data: [],
+    loading: false,
+    error: null,
+    increment: () => set((state) => ({ count: state.count + 1 })),
+    decrement: () => set((state) => ({ count: state.count - 1 })),
+    setName: (value) => set((state) => ({ username: state.username + value })),
+    addProduct: (product) => {set(produce((state) => {state.products.push(product)}))},
 
+    voteHandler: (id) => {
+      set(produce((state)=>{
+        let clickedVote = state.colors.find((color)=> id === color.id)
+        clickedVote.vote += 1
 
-
+      }))
+    },
+    fetchData: async () => {
+        set(()=>({loading: true}))
+        try {
+            let response = await fetch("https://fakestoreapi.com/products");
+            let fetchData = await response.json()
+            set(()=>({data: fetchData, loading: false}))
+        } catch (error) {
+            set(()=>({error: error.message, loading: false}))
+        }
     }
-})
+  };
+});
